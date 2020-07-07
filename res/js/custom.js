03071789c5e2992ba291501ea818f1cf4204eabf
Jolly Good <1671375+good-lly@users.noreply.github.com>
2020-07-07 18:32:10 +0200

    parsing content from remote gsheet csv

--- res/js/custom.js
+++ res/js/custom.js
@@ -1,8 +1,59 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-    var elem = document.querySelector('.grid');
-    var iso = new Isotope(elem, {
+    var grid = document.getElementById('js-grid');
+    var filterButtonGroup = document.getElementById('js-buttongroup');
+    var tags = [];
+    var iso = new Isotope(grid, {
         // options
         itemSelector: '.grid-item',
         layoutMode: 'fitRows',
     });
+    function addTags(rowTags) {
+        rowTagsArr = rowTags.split(',');
+        rowTagsArr.forEach((element) => {
+            if (tags.indexOf(element.trim()) == -1) {
+                tags.push(element);
+            }
+        });
+    }
+    function createNewEntry(data) {
+        var node = document.createElement('A');
+        node.setAttribute('href', data.url);
+        var img = document.createElement('div');
+        img.classList.add('img');
+        img.style.backgroundImage = "url('" + data.icon + "')";
+        node.appendChild(img);
+        var title = document.createElement('H5');
+        var titleText = document.createTextNode(data.title);
+        title.appendChild(titleText);
+        node.appendChild(title);
+        var subtitle = document.createElement('P');
+        var subtitleText = document.createTextNode(data.subtitle);
+        subtitle.appendChild(subtitleText);
+        node.appendChild(subtitle);
+        var tags = data.types.split(',');
+        node.classList.add('grid-item');
+        tags.forEach((elm) => {
+            node.classList.add(elm);
+        });
+        grid.appendChild(node);
+        iso.appended(node);
+    }
+    Papa.parse(
+        'https://docs.google.com/spreadsheets/d/1ymM6CpLo_AORup4SV-yQTNNFabjqVNWSPUu8JnUqM5o/gviz/tq?tqx=out:csv&sheet=czech',
+        {
+            header: true,
+            download: true,
+            step: function (row) {
+                console.log('Row:', row.data);
+                if (row.data !== undefined) {
+                    createNewEntry(row.data);
+                    if (row.data.types !== undefined) addTags(row.data.types);
+                }
+            },
+            complete: function () {
+                iso.layout();
+                console.warn('complete');
+            },
+        }
+    );
 });
